b69cda7b4cd96de525710e882d62cc4affd8993e
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2022-12-07 11:17:46 +0530

    #include<stdio.h> int main() {     int n,j,i;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=i;j++)         {             if(j==1||i==n||i==j)             {                 printf("*");             }             else             {                 printf(" ");             }         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,21 +1,22 @@
 #include<stdio.h>
 int main()
 {
-    int a,i;
-    scanf("%d",&a);
-    int t=a;
-    for(i=1; i<=a; i++){
-        
-while(a!=0){
-
-    printf("%d ",a);
-    a--;
-    
-}
-printf("
+    int n,j,i;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            if(j==1||i==n||i==j)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("
 ");
-a=t;
-
-}
-    
+    }
 }
