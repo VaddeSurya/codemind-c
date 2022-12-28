9a6a61872bc20f84dfea0df784c4a9abcb1dcd4e
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2022-12-28 18:46:49 +0530

    #include<stdio.h> int main() { 	int a,b,c,d; 	scanf("%d%d",&a,&b); 	c=a+b; 	d=a-b; 	printf("%d %d ",c,d); 	float x,y,e,f; 	scanf("%f%f",&x,&y); 	e=x+y; 	f=x-y; 	printf("%.1f %.1f",e,f); }

---
+++ Sum_and_Difference.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d;
+	scanf("%d%d",&a,&b);
+	c=a+b;
+	d=a-b;
+	printf("%d %d
+",c,d);
+	float x,y,e,f;
+	scanf("%f%f",&x,&y);
+	e=x+y;
+	f=x-y;
+	printf("%.1f %.1f",e,f);
+}
