301b40d810cbdf143c939b03e4e9a5b4f373bbf4
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2023-08-02 14:12:19 +0530

    #include<stdio.h> int main() {     int n,digit,large=0;     scanf("%d",&n);     while(n>0)     {         digit=n%10;         if(digit>large)         {             large=digit;         }         n=n/10;     }     printf("%d",large); }

--- Largest_Digit.c
+++ Largest_Digit.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int n,r,m=0;
+    int n,digit,large=0;
     scanf("%d",&n);
     while(n>0)
     {
-        r=n%10;
-        if(m<r)
-        m=r;
+        digit=n%10;
+        if(digit>large)
+        {
+            large=digit;
+        }
         n=n/10;
-        
     }
-    printf("%d",m);
+    printf("%d",large);
 }
