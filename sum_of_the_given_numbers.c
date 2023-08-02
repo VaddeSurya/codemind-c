81bb7619905151e23021132052c87a2c503bec98
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2023-08-02 15:04:51 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     for(i=0;i<n;i++)     {         int a,b;         scanf("%d %d",&a,&b);         printf("%d ",a+b);     }     return 0; }

--- sum_of_the_given_numbers.c
+++ sum_of_the_given_numbers.c
@@ -1,14 +1,14 @@
-#include<iostream>
-using namespace std;
+#include<stdio.h>
 int main()
 {
-	int n,i;
-	cin >> n;
-	for (i = 0; i < n; i++)
-	{
-		int a, b;
-		cin >> a >> b;
-		cout << a + b<< endl;
-	}
-	return 0;
+    int n,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        int a,b;
+        scanf("%d %d",&a,&b);
+        printf("%d
+",a+b);
+    }
+    return 0;
 }
