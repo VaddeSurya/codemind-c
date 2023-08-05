c130ab8d65a7dbcfd2759c8718b844abea0ed126
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2023-08-06 02:06:18 +0530

    #include<iostream> using namespace std;
    int main() {
        int n, j;
        cin >> n >> j;
        for (int i = 1; i <= j; i++) {
            cout << n << " x " << i << " = " << n * i << endl;
        }
        return 0;
    }

--- Multiplication_Table_2.c
+++ Multiplication_Table_2.c
@@ -1,9 +1,11 @@
-#include<stdio.h>
-int main()
-{
-    int n,i,j;
-    scanf("%d%d",&n,&j);
-    for(i=1;i<=j;i++)
-    printf("%d x %d = %d
-",n,i,n*i);
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n, j;
+    cin >> n >> j;
+    for (int i = 1; i <= j; i++) {
+        cout << n << " x " << i << " = " << n * i << endl;
+    }
+    return 0;
 }
