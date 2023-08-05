242f41735aba60a1a2c7a2e11b7d73d2955f3762
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2023-08-05 18:33:32 +0530

    #include <iostream>
    using namespace std;
    
    int main() {
        int n;
        scanf("%d",&n);
        int arr[n];
    
        for (int i = 0; i < n; ++i) {
            cin >> arr[i];
        }
        int sum = 0;
        for (int i = 0; i < n; ++i) {
            if (arr[i] % 2 == 0) {
                sum += arr[i];
            }
        }
        printf("%d",sum);
        return 0;
    }

--- Even_Sum.c
+++ Even_Sum.c
@@ -1,19 +1,21 @@
-#include<stdio.h>
-int main()
-{
-    int n,i,sum=0;
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
     scanf("%d",&n);
-    int a[n];
-    for(i=0;i<n;i++)
-    {
-        scanf("%d",&a[i]);
+    int arr[n];
+
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
     }
-    for(i=0;i<n;i++)
-    {
-        if(a[i]%2==0)
-        {
-            sum=sum+a[i];
+    int sum = 0;
+    for (int i = 0; i < n; ++i) {
+        if (arr[i] % 2 == 0) {
+            sum += arr[i];
         }
     }
     printf("%d",sum);
+    return 0;
 }
