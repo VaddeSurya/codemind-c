372f0396ced8dac1c829bb6de5ab332f585d9b5b
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2023-08-05 18:39:50 +0530

    #include <iostream> using namespace std; int main() {     int n;     scanf("%d",&n);     int arr[n];     for (int i = 0; i < n; ++i) {         scanf("%d",&arr[i]);     }     int sum = 0;     for (int i = 0; i < n; ++i) {             sum += arr[i];         }     printf("%d",sum);     return 0; }

--- Sum_of_Array.c
+++ Sum_of_Array.c
@@ -1,16 +1,16 @@
-#include<stdio.h>
-int main()
-{
-    int n,sum=0;
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
     scanf("%d",&n);
-    int a[n],i;
-    for(i=0;i<n;i++)
-    {
-        scanf("%d",&a[i]);
+    int arr[n];
+    for (int i = 0; i < n; ++i) {
+        scanf("%d",&arr[i]);
     }
-    for(i=0;i<n;i++)
-    {
-        sum=sum+a[i];
-    }
+    int sum = 0;
+    for (int i = 0; i < n; ++i) {
+            sum += arr[i];
+        }
     printf("%d",sum);
+    return 0;
 }
