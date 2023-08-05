257d824253ff5b9c0aae2a2e179c70c241e630e6
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2023-08-05 18:32:37 +0530

    #include <iostream>
    using namespace std;
    
    int main() {
        int n;
        scanf("%d",&n);
        int arr[n];
    
        for (int i = 0; i < n; ++i) {
            cin >> arr[i];
        }
        int sum = 0;
        for (int i = 0; i < n; ++i) {
            if (arr[i] % 2 != 0) {
                sum += arr[i];
            }
        }
        printf("%d",sum);
        return 0;
    }

---
+++ Odd_Sum.c
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+    int sum = 0;
+    for (int i = 0; i < n; ++i) {
+        if (arr[i] % 2 != 0) {
+            sum += arr[i];
+        }
+    }
+    printf("%d",sum);
+    return 0;
+}
