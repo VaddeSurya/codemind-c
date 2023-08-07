243ba764377062a03f8b802e571008da1821cb7c
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2023-08-07 21:31:32 +0530

    #include<iostream> using namespace std; int main() {     int n,sum1=0,dig;     cin>>n;     int sq=n*n;     while(sq>0)     {     dig=sq%10;     sum1+=dig;     sq/=10;     }     if(n==sum1)     {         cout<<"Neon Number";     }     else     {         cout<<"Not Neon Number";     } }

--- Neon_Number.c
+++ Neon_Number.c
@@ -1,21 +1,22 @@
-#include<stdio.h>
+#include<iostream>
+using namespace std;
 int main()
 {
-    int n,sq,i,sum=0;
-    scanf("%d",&n);
-    sq=n*n;
-    while(sq!=0)
+    int n,sum1=0,dig;
+    cin>>n;
+    int sq=n*n;
+    while(sq>0)
     {
-        i=sq%10;
-        sum=sum+i;
-        sq=sq/10;
+    dig=sq%10;
+    sum1+=dig;
+    sq/=10;
     }
-    if(sum==n)
+    if(n==sum1)
     {
-        printf("Neon Number");
+        cout<<"Neon Number";
     }
     else
     {
-        printf("Not Neon Number");
+        cout<<"Not Neon Number";
     }
 }
