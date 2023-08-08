baca6142cac4cab0f71e572c17f317d7089043de
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2023-08-08 21:34:53 +0530

    #include<iostream> using namespace std; int main() {     int arr[100],i,n,sum = 0,aver,count=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);         sum += arr[i];     }     aver=sum/n;     for(i=0;i<n;i++)     {         if(arr[i]>=aver)         {             count+=1;         }     }     printf("%d",count); }

---
+++ Elements_greater_than_average.c
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[100],i,n,sum = 0,aver,count=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        sum += arr[i];
+    }
+    aver=sum/n;
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]>=aver)
+        {
+            count+=1;
+        }
+    }
+    printf("%d",count);
+}
