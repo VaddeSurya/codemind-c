6a07cda87d566c16bfe0ba7ffd0c59c256ed5f85
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2023-08-08 21:04:37 +0530

    #include<iostream> using namespace std; int main() {     int arr[100],n,i;     scanf("%d",&n);     bool is_even=true;     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);         if(arr[i]%2!=0)         {             is_even=false;             break;         }     }     if(is_even)     {         printf("True");     }     else     {         printf("False");     } }

--- Even_Array.c
+++ Even_Array.c
@@ -3,11 +3,11 @@ using namespace std;
 int main()
 {
     int arr[100],n,i;
-    cin>>n;
+    scanf("%d",&n);
     bool is_even=true;
     for(i=0;i<n;i++)
     {
-        cin>>arr[i];
+        scanf("%d",&arr[i]);
         if(arr[i]%2!=0)
         {
             is_even=false;
@@ -16,10 +16,10 @@ int main()
     }
     if(is_even)
     {
-        cout<<"True";
+        printf("True");
     }
     else
     {
-        cout<<"False";
+        printf("False");
     }
 }
